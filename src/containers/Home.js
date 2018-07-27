d1e86d4861ee4afc35a118e0d6563ab9b701806b
mariouvega <mario.vega014@gmail.com>
2018-07-27 13:03:43 -0400

    added react hot loader to app

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -8,9 +8,25 @@ const Image = styled.img`
 `
 
 class Home extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			data: 0,
+		}
+	}
+
+	increment() {
+		this.setState({
+			data: this.state.data + 1
+		});
+	}
+
   render() {
     return (
       <Container>
+	      <h1>Hello!!!!!!!!!!!!!!!!!!!!</h1>
+	      <p>{this.state.data}</p>
+	      <button onClick={this.increment.bind(this)}>+</button>
         <Image src={require('../assets/VegaMarioCopy.jpg')} alt="Mario Vega" />
         <p>
         	Lo-fi microdosing pickled umami brunch. Man braid taiyaki photo booth, DIY gastropub trust fund brooklyn chambray twee VHS. Lomo cornhole raclette, venmo chia neutra narwhal street art twee poke. Semiotics street art selfies meditation cray migas.
--- src/index.js
+++ src/index.js
@@ -3,6 +3,23 @@ import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
 import routes from './routes';
 import './theme/globalStyle';
+import { AppContainer } from 'react-hot-loader';
 
-ReactDOM.render(routes, document.getElementById('root'));
+ReactDOM.render(
+	<AppContainer>
+		{routes}
+	</AppContainer>, document.getElementById('root'));
 registerServiceWorker();
+
+// Hot Mudule Replacement API
+if (module.hot) {
+	module.hot.accept('./routes', () => {
+		const routes = require('./routes').default
+		ReactDOM.render(
+			<AppContainer>
+				{routes}
+			</AppContainer>,
+			document.getElementById('root')
+			);
+	});
+}
