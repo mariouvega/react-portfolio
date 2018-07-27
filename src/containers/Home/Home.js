ddedaaa44ca36c549de823624f5ccf48e881e44e
mariouvega <mario.vega014@gmail.com>
2018-07-27 16:16:49 -0400

    checking out to new branch and experimenting with webpack

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,36 +1,23 @@
 import React, { Component, PropTypes } from 'react';
 import styled from 'styled-components';
-import Container from '../theme/grid/Container';
+import { Container } from '../../theme/grid';
+import { MyImage, RevealP } from './Home.style';
 
-
-const Image = styled.img`
-	width: 100%;
-`
 
 class Home extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			data: 0,
-		}
-	}
-
-	increment() {
-		this.setState({
-			data: this.state.data + 1
-		});
-	}
+	static propTypes = {}
 
   render() {
     return (
       <Container>
-	      <h1>Hello!!!!!!!!!!!!!!!!!!!!</h1>
-	      <p>{this.state.data}</p>
-	      <button onClick={this.increment.bind(this)}>+</button>
-        <Image src={require('../assets/VegaMarioCopy.jpg')} alt="Mario Vega" />
-        <p>
+      	<MyImage>
+      		<h1>Mario Vega</h1>
+      		<h2>Full Stack Software Engineer</h2>
+      	</MyImage>
+        {/*<Image src={require('../../assets/VegaMarioCopy.jpg')} alt="Mario Vega" />*/}
+        <RevealP>
         	Lo-fi microdosing pickled umami brunch. Man braid taiyaki photo booth, DIY gastropub trust fund brooklyn chambray twee VHS. Lomo cornhole raclette, venmo chia neutra narwhal street art twee poke. Semiotics street art selfies meditation cray migas.
-        </p>
+        </RevealP>
       </Container>
     );
   }
