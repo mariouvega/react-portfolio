9e2e7a08b33d08047a96c5a4364be6808a986dc2
mariouvega <mario.vega014@gmail.com>
2018-07-29 23:58:36 -0400

    added color variables for app

---
+++ src/containers/App/App.style.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+import { lightBlue, purple, grey, blue } from 'theme/variables';
+
+
+export const Background = styled.div`
+	position: fixed;
+	left: 10vw;
+	width: 80vw;
+	top: 10vh;
+	height: 80vh;
+	// background-color: ${purple};
+	// opacity: 0.4;
+	background-size: 80%;
+	background-repeat: no-repeat;
+	background-position: center;
+	z-index: -99;
+`
---
+++ src/theme/variables.js
@@ -0,0 +1,5 @@
+export const grey = '#727072';
+export const blue = '#30323d';
+export const lightBlue = '#c7d3dd';
+export const purple = '#544e61';
+export const black = '#000';
